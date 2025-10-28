f48aa20cd0f28f199954558d1131c0eca67046b6
sami0100 <167051023+sami0100@users.noreply.github.com>
2025-10-28 19:09:29 +0000

    final commit

--- addressbook/app.js
+++ addressbook/app.js
@@ -1,13 +1,13 @@
-
 var express = require('express');
 var app = express();
 
+// Set up middleware, routes, etc.
 app.get('/', function(req, res) {
   res.send('Hello World!');
 });
-var personsRouter = require('./routes/persons'); // Import the persons routes
 
-// Use the persons routes for any URL that starts with /persons
-app.use('/persons', personsRouter);
+var personsRouter = require('./routes/persons');  // Import the persons routes
 
-module.exports = app;
+app.use('/persons', personsRouter);  // Use the persons routes for any URL that starts with /persons
+
+module.exports = app;  // Export the app object

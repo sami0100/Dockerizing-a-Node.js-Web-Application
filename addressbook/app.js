88399eeb59eb54f574d5996f099c8900ef62ae35
sami0100 <167051023+sami0100@users.noreply.github.com>
2025-10-28 18:42:02 +0000

    Implemented CRUD routes and tested

--- addressbook/app.js
+++ addressbook/app.js
@@ -1,5 +1,10 @@
+
 var express = require('express');
 var app = express();
+
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
 var personsRouter = require('./routes/persons'); // Import the persons routes
 
 // Use the persons routes for any URL that starts with /persons
